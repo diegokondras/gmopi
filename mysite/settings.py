f4a742b61cdab3cd9bd083919102515578c7df1e
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-10-10 19:30:41 -0300

    Projeto Final

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,7 +92,9 @@ DATABASES = {
     'default': {		'default': {
         'ENGINE': 'django.db.backends.sqlite3',	    	'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),	    	'NAME': 'gmopi',
+        }
     }
+}
 
 
 
