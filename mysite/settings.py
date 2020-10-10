ab95799698a4b5b51467ee4a8f6ce4db1002f9bb
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-10-10 18:04:32 -0300

    Projeto Final

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,10 +89,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+    	'ENGINE': 'django.db.backends.mysql',
+    	'NAME': 'gmopi',
+    	'USER': 'diegoeduardokond',
+    	'PASSWORD': '#Ifsc2020',
+    	'HOST': 'localhost',
+    	'PORT': '',
+	}
 }
 
 
