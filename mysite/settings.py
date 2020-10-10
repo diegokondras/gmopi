320802064be5da26b853a016f51b78e7efb5cc5c
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-10-10 19:23:47 -0300

    Projeto Final

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,16 +88,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
-DATABASES = {
-	'default': {
-    	'ENGINE': 'django.db.backends.mysql',
-    	'NAME': 'gmopi',
-    	'USER': 'diegoeduardokond',
-    	'PASSWORD': '#Ifsc2020',
-    	'HOST': 'localhost',
-    	'PORT': '',
-	}
-}
+DATABASES = {	DATABASES = {
+    'default': {		'default': {
+        'ENGINE': 'django.db.backends.sqlite3',	    	'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),	    	'NAME': 'gmopi',
+    }
 
 
 
