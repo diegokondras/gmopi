5b94adcc9784eb0c9f7b2d0c28aef5fc2561fac3
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-10-10 19:27:16 -0300

    Projeto Final

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
-DATABASES = {	DATABASES = {
+DATABASES = {
     'default': {		'default': {
         'ENGINE': 'django.db.backends.sqlite3',	    	'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),	    	'NAME': 'gmopi',
