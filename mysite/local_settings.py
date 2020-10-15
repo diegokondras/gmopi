8659bc4bdf629ed4473cf73f050e8f526491059f
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-10-15 16:17:00 -0300

    Projeto Final

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,11 +1,9 @@
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3)dhrdpzmc@xl-ftsj9$e7f4qk8#d$9adhg&y8e*c)rerf@-g!'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
-DB_PASSWORD = 'projetofinal'
-DB_USER = 'admin'
-DB_NAME = 'gmopi'
-DB_HOST = 'localhost'
+DEBUG = False
+DB_PASSWORD = 'ifsc2020'
+DB_USER = 'gmopiifsc'
+DB_NAME = 'gmopiifsc$gmopi'
+DB_HOST = 'gmopiifsc.mysql.pythonanywhere-services.com'
