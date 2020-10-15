7c660953ad34dd2c67be32067b4b67b5d22aa8c0
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-10-15 15:36:14 -0300

    Projeto Final

---
+++ mysite/local_settings.py
@@ -0,0 +1,11 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '3)dhrdpzmc@xl-ftsj9$e7f4qk8#d$9adhg&y8e*c)rerf@-g!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+DB_PASSWORD = 'projetofinal'
+DB_USER = 'admin'
+DB_NAME = 'gmopi'
+DB_HOST = 'localhost'
